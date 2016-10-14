771d44ece392592a23f3fa81f42dc5ab42f080b1
root <root@livecd.gentoo>
2016-10-14 21:51:41 +0000

    fix of runx.sh support for e4crypt home

--- rootfs/usr/local/bin/runx.sh
+++ rootfs/usr/local/bin/runx.sh
@@ -4,6 +4,4 @@ if [ $(echo $TTY | grep '/dev/tty') ] ;then
     TTYNUM=$(echo $TTY | grep '/dev/tty' | sed 's/\/dev\/tty//')
 fi
 
-if [ "${TTYNUM}X" != "X" ]
-    exec xinit -display $TTYNUM
-fi
+[ "${TTYNUM}X" != "X" ] && exec startx -- :${TTYNUM} vt${TTYNUM}
