a7e4d7e0a29d60b9e8b03791724fcbc977a3946d
aggi <aggi@localhost>
2016-10-17 22:21:35 +0000

    do not mount backup automatically

--- rootfs/etc/rc.local.d/cryptsetup.sh
+++ rootfs/etc/rc.local.d/cryptsetup.sh
@@ -6,8 +6,8 @@ if [ -e "/dev/disk/by-id/md-uuid-6aef3b48:44a41291:1a0f817e:c5fed5f1" ]; then
 	cryptsetup open "/dev/disk/by-id/md-uuid-6aef3b48:44a41291:1a0f817e:c5fed5f1" home --type plain --cipher aes-xts-plain --key-size 512 --hash sha512
 fi
 
-if [ -e "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c" ]; then
-	cryptsetup close backup
-	echo "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c"
-	cryptsetup open "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c" backup --type plain --cipher aes-xts-plain64 --key-size 512 --hash sha512
-fi
+#if [ -e "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c" ]; then
+#	cryptsetup close backup
+#	echo "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c"
+#	cryptsetup open "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c" backup --type plain --cipher aes-xts-plain64 --key-size 512 --hash sha512
+#fi
