cf04172c6b85a4a54f63d741c4d369fd79b8c2e9
aggi <aggi@localhost>
2016-10-17 22:20:04 +0000

    fixed cryptsetup.sh script

--- rootfs/etc/rc.local.d/cryptsetup.sh
+++ rootfs/etc/rc.local.d/cryptsetup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ -e "/dev/disk/by-id/md-uuid-6aef3b48:44a41291:1a0f817e:c5fed5f1" ]; then
+	cryptsetup close home
 	echo "/dev/disk/by-id/md-uuid-6aef3b48:44a41291:1a0f817e:c5fed5f1"
 	cryptsetup open "/dev/disk/by-id/md-uuid-6aef3b48:44a41291:1a0f817e:c5fed5f1" home --type plain --cipher aes-xts-plain --key-size 512 --hash sha512
 fi
 
 if [ -e "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c" ]; then
+	cryptsetup close backup
 	echo "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c"
 	cryptsetup open "/dev/disk/by-id/scsi-3600508e000000000d29b2a53dca8050c" backup --type plain --cipher aes-xts-plain64 --key-size 512 --hash sha512
 fi
-
-exit 0
