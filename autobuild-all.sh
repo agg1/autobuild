b53a43b617d0759a4d87818c8690eeb72b1d2e1c
aggi <aggi@localhost>
2016-10-17 04:46:45 +0000

    set parameters in autobuild scripts

--- autobuild-all.sh
+++ autobuild-all.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 # Copyright aggi 2016
 
-set -e
+export MAKEOPTS="${MAKEOPTS:--j12}"
+export STAMP="${STAMP:-latest}"
+export TARGT=""
+export CCONF="${CCONF:-/home/catalyst/catalyst-cache.conf}"
+export CADIR="/home/catalyst"
+export RELDA="20161014"
+export BDDIR="${BDDIR:-/var/tmp/catalyst/builds/hardened}"
+export SDDIR="${SDDIR:-/home/seeds}"
+export PKDIR="${PKDIR:-/home/packages}"
+export DFDIR="${DFDIR:-/home/distfiles}"
+export PTREE="${SDDIR}/portage.tar"
+#export PTREE="${PTREE:-${SDDIR}/portage-latest.tar.bz2}"
+export RODIR="${RODIR:-${CADIR}/rootfs}"
 
 source /home/catalyst/autobuild.sh
 
