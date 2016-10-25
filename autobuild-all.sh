11c972c21e17af635a5f4b685ce448f7611c503d
root <root@livecd.gentoo>
2016-10-25 10:43:07 +0000

    package dir paths

--- autobuild-all.sh
+++ autobuild-all.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -e
 # Copyright aggi 2016
 
-export PKDIR="/home/packages-desktop"
-
 source /home/catalyst/autobuild.sh
 prepare_system
 
@@ -10,5 +8,7 @@ clean_portage
 prepare_portage
 build_seed_init
 build_seed
+export PKDIR="/home/packages-minimal"
 build_livecd_minimal
+export PKDIR="/home/packages-desktop"
 build_livecd_desktop
