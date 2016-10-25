1862ace22f3611762fdcebe66863adc0582f6285
root <root@livecd.gentoo>
2016-10-25 10:42:42 +0000

    fixed autobuild-update.sh

--- autobuild-update.sh
+++ autobuild-update.sh
@@ -1,25 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 # Copyright aggi 2016
 
-export MAKEOPTS="${MAKEOPTS:--j12}"
-export STAMP="${STAMP:-latest}"
-export TARGT=""
-export CCONF="${CCONF:-/home/catalyst/catalyst-cache.conf}"
-export CADIR="/home/catalyst"
-export RELDA="20161014"
-export BDDIR="${BDDIR:-/var/tmp/catalyst/builds/hardened}"
-export SDDIR="${SDDIR:-/home/seeds}"
-export PKDIR="${PKDIR:-/home/packages}"
-export DFDIR="${DFDIR:-/home/distfiles}"
-export PTREE="${PTREE:-${SDDIR}/portage-latest.tar.bz2}"
-export RODIR="${RODIR:-${CADIR}/rootfs}"
-
 source /home/catalyst/autobuild.sh
+prepare_system
 
-prepare_system && \
-clean_portage && \
-prepare_portage && \
-build_livecd_minimal && \
-update_livecd_desktop && \
-echo "SUCCESS" || \
-echo "ERROR"
+clean_portage
+prepare_portage
+export PKDIR="/home/packages-minimal"
+build_livecd_minimal
+export PKDIR="/home/packages-desktop"
+update_livecd_desktop
