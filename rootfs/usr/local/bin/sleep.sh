07cb6b280ed60a4b6ce44a4e831fb149ec52a20a
root <root@livecd.gentoo>
2016-10-27 05:19:04 +0000

    allow for ssh session disconnect with sleep.sh

--- rootfs/usr/local/bin/sleep.sh
+++ rootfs/usr/local/bin/sleep.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo 3>/proc/sys/vm/drop_caches
 sync
-echo mem > /sys/power/state
+echo "suspending in 10 seconds... consider closing ssh session"
+sleep 10
+nohup echo mem > /sys/power/state
