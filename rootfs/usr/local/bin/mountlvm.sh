6c9352604d4bbb87c9383b57e205966a0db2facd
root <root@livecd.gentoo>
2016-10-27 05:19:38 +0000

    fixed lvm setup and mount

--- rootfs/usr/local/bin/mountlvm.sh
+++ rootfs/usr/local/bin/mountlvm.sh
@@ -2,12 +2,12 @@
 
 cryptsetup open /dev/md127 home --type plain --cipher aes-xts-plain64 --key-size 512 --hash sha512
 
-if [ ! -e /dev/disk/by-label/HOME ];then
+/sbin/vgs | grep vghome
+if [ $? -gt 0 ];then
 	cryptsetup close home
 	echo "setup error"
 fi
 
-#
 fsck -y /dev/mapper/vghome-lvhome
 mount -o nodev,nosuid,noexec /dev/mapper/vghome-lvhome /home/ 2>/dev/null
 for i in $(ls /dev/mapper/vghome-lv*) ; do
