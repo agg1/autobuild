bd37f3585211e692862497dade946af58a655311
root <root@localhost>
2016-10-30 08:35:34 +0000

    removed binaries from local/bin now shipped with extra_overlay

--- rootfs/usr/local/bin/mountlvm.sh
+++ rootfs/usr/local/bin/mountlvm.sh
@@ -10,10 +10,12 @@ if [ $? -gt 0 ];then
 fi
 
 fsck -y /dev/mapper/vghome-lvhome
+fsck -y /dev/mapper/vghome-lvdistfiles
 mount -o nodev,nosuid,noexec /dev/mapper/vghome-lvhome /home/ 2>/dev/null
-for i in $(ls /dev/mapper/vghome-lv* | grep -v lvhome | grep -v lvcatalyst | grep -v lvfw01 | grep -v lvwin01) ; do
+mount -o nodev,nosuid,noexec /dev/mapper/vghome-lvdistfiles /home/distfiles 2>/dev/null
+for i in $(ls /dev/mapper/vghome-lv* | grep -v lvhome | grep -v lvdistfiles | grep -v lvfw01 | grep -v lvwin01) ; do
 	LVOL=$(basename $i | cut -d'-' -f2)
-	fsck -y /dev/mapper/$LVOL
+	fsck -y $i
 	mount -o noatime,nodev,nosuid,noexec $i /media/lvm/$LVOL 2>/dev/null
 	# -o user_xattr,sync,dirsync
 done
