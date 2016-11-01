0d7fd23605e5b7c5ea615d5af8eef9f67914ebae
Your Name <you@example.com>
2016-11-01 19:31:26 +0000

    minor script fixes

--- rootfs/usr/local/bin/privoxy.sh
+++ rootfs/usr/local/bin/privoxy.sh
@@ -1,2 +1,2 @@
-sg wanout -c "systrace -d /usr/local/etc/systrace -a /usr/sbin/privoxy -- --pidfile /var/run/privoxy.pid --user privoxy.privoxy /usr/local/etc/privoxy/config "&
+systrace -d /usr/local/etc/systrace -a /usr/sbin/privoxy -- --pidfile /var/run/privoxy.pid --user privoxy.privoxy /usr/local/etc/privoxy/config&
 
--- rootfs/usr/local/bin/tor.sh
+++ rootfs/usr/local/bin/tor.sh
@@ -1,2 +1,2 @@
-sg wanout -c "systrace -d /usr/local/etc/systrace -a /usr/bin/tor -- --PidFile /var/run/tor.pid --defaults-torrc /usr/local/etc/tor/torrc "&
+sg wanout -c "systrace -d /usr/local/etc/systrace -a /usr/bin/tor -- --defaults-torrc /usr/local/etc/tor/torrc "&
 
