4d4e455063de998f19c6f96a20edd6d84d2533af
aggi <aggi@localhost>
2016-11-10 15:54:55 +0000

    fixed mountlvm script

--- rootfs/usr/local/bin/mountlvm.sh
+++ rootfs/usr/local/bin/mountlvm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cryptsetup open /dev/md127 home --type plain --cipher aes-xts-plain64 --key-size 512 --hash sha512
+pvscan -a ay --cache
 
 /sbin/vgs | grep vghome
 if [ $? -gt 0 ];then
