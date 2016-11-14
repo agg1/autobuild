334f4f8328b80b6d62924bd3182474dc5e06ac9c
aggi <aggi@localhost>
2016-11-14 21:16:15 +0000

    archive digests

--- autobuild-update.sh
+++ autobuild-update.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
 # Copyright aggi 2016
-
+PTREE=/home/seeds/gentoo/portage-hardened.tar
 source /home/catalyst/autobuild.sh
 prepare_system
 
 clean_portage
 prepare_portage
 
-#export PKDIR="/home/packages-minimal"
-#update_livecd_minimal
+export PKDIR="/home/packages-minimal"
+update_livecd_minimal
 export PKDIR="/home/packages-admin"
 update_livecd_admin
 export PKDIR="/home/packages-desktop"
 update_livecd_desktop
+
+archive_digests
