1372d1866e697676f93c3443821a24d8156095b8
aggi <aggi@localhost>
2016-11-14 21:12:54 +0000

    cleanup prepare update scripts

--- rootfs/usr/local/bin/preparesysupdate.sh
+++ rootfs/usr/local/bin/preparesysupdate.sh
@@ -1,24 +1,9 @@
 #!/bin/sh -e
 
 [ ! -e /home/seeds/portage/latest/portage-latest.tar.bz2 ] && echo "latest portage tree not found" && exit 1
-[ -e /etc/.readonly ] && mount -o remount,rw /etc
 
 /usr/local/bin/writable.sh /mnt/livecd
 cd /usr ; tar -xf /home/seeds/portage/latest/portage-latest.tar.bz2
-
-[ ! -e /etc/portage.orig ] && cp -pR /etc/portage /etc/portage.orig
-rm -rf /etc/portage
-cp -pr /home/catalyst/etc/portage /etc/portage
-rm -f /etc/portage/make.profile
-
-cd -
-
-cd /home/catalyst
-rm -f etc/portage/make.profile
-ln -sf ../../usr/portage/profiles/hardened/linux/amd64/no-multilib /etc/portage/make.profile
-ln -sf ../../usr/portage/profiles/hardened/linux/amd64/no-multilib etc/portage/make.profile
-
-cd -
 
 mount -o remount,exec /tmp
 mount -o remount,exec /var/tmp
@@ -28,4 +13,3 @@ mount --bind /home/distfiles /usr/portage/distfiles
 
 #mkdir -p /usr/portage/packages
 #mount --bind /home/packages /usr/portage/packages
-
