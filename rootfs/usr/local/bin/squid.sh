f4b6e83f76640f2381c8d9ad614e6cc6b11f0bba
aggi <aggi@localhost>
2016-11-14 13:46:35 +0000

    fixed squid startup script

--- rootfs/usr/local/bin/squid.sh
+++ rootfs/usr/local/bin/squid.sh
@@ -2,6 +2,9 @@ mkdir -p /tmp/squid/
 chown squid:squid /tmp/squid/
 chmod 700 /tmp/squid/
 htpasswd -b -c /tmp/squid/passwd squid squid0815
+rm -rf /var/cache/squid/*
 /usr/sbin/squid -f /usr/local/etc/squid/squid.conf -z
+sync
+sleep 3
 
 systrace -d /usr/local/etc/systrace -a /usr/sbin/squid -- -f /usr/local/etc/squid/squid.conf -s &
