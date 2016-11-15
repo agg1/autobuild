1b8d24e19385ca4a814721f841a2887918a26d6a
aggi <aggi@localhost>
2016-11-15 18:14:20 +0000

    fixed clearssd to set passw

--- rootfs/usr/local/bin/clearssd.sh
+++ rootfs/usr/local/bin/clearssd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DISK=$1
 [ -z "${DISK}" -o ! -e "${DISK}" ] && echo "disk error" && exit
-hdparm --user-master u --security-set-pass $DISK
-hdparm --user-master u --security-erase $DISK
+hdparm --user-master u --security-set-pass pass $DISK
+hdparm --user-master u --security-erase pass $DISK
