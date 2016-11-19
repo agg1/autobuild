cc6d3d66d0c0f16c98ae2a599d19823145c25bbd
aggi <aggi@localhost>
2016-11-19 12:58:54 +0000

    updated backup script

--- autobuild-backup.sh
+++ autobuild-backup.sh
@@ -6,4 +6,8 @@ mount $DISK /media/stick
 /usr/local/bin/unlockdir.sh /media/stick/container
 
 rm -rf /media/stick/container/catalyst
-cp -pR /home/catalyst /media/stick/container
+cp -va /home/catalyst /media/stick/container
+rm -f $(find /media/stick/container/seeds/ | grep latest)
+cp -va /home/seeds /media/stick/container
+rm -rf /media/stick/container/packages*
+cp -va /home/packages* /media/stick/container
