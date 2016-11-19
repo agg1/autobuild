b66ce3d7783cbebd5444e16e2adcafdcfd6f3629
aggi <aggi@localhost>
2016-11-19 17:30:48 +0000

    fixed update prepare scripts to accept PTREE parameter

--- rootfs/usr/local/bin/preparesysupdate.sh
+++ rootfs/usr/local/bin/preparesysupdate.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -e
 
-[ -e /mnt/livecd/.writeable ] && echo "already prepared" && exit 1
+PTREE=$1
+[ -z "${PTREE}" -o ! -e "${PTREE}" ] && echo "latest portage tree not found" && exit 1
 
-[ ! -e /home/seeds/portage/latest/portage-latest.tar.bz2 ] && echo "latest portage tree not found" && exit 1
+[ -e /usr/.writeable ] && echo "already prepared" && exit 1
 
 mount -o remount,rw /etc 2> /dev/null
 mount -o remount,exec /tmp 2> /dev/null
 mount -o remount,exec /var/tmp 2> /dev/null
 
 /usr/local/bin/writable.sh /mnt/livecd
-cd /usr ; tar -xf /home/seeds/portage/latest/portage-latest.tar.bz2
+cd /usr ; tar -xf ${PTREE}
 
 mkdir -p /usr/portage/distfiles
 mount --bind /home/distfiles /usr/portage/distfiles
