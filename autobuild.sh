a7e31570e9800e55e0864e9b356e8be974d6da85
aggi <aggi@localhost>
2016-11-22 03:07:16 +0000

    do not symlink latest instead set LATEST env variable to point to desired seed stages

--- autobuild.sh
+++ autobuild.sh
@@ -8,7 +8,8 @@ prepare_system() {
 	mount -o remount,exec,dev,suid,size=22G /tmp/ || true
 	mount -o remount,exec,dev,suid,size=22G /var/tmp/ || true
 
-	NEWDA="$(date +%Y%m%d-%s)"
+	export LATEST="${LATEST:-latest}"
+	export NEWDA="$(date +%Y%m%d-%s)"
 	export MAKEOPTS="${MAKEOPTS:--j12}"
 	export STAMP="${STAMP:-latest}"
 	export TARGT=""
@@ -21,7 +22,7 @@ prepare_system() {
 	export SDDIR="${SDDIR:-/home/seeds}"
 	export PKDIR="${PKDIR:-/home/packages}"
 	export DFDIR="${DFDIR:-/home/distfiles}"
-	export PTREE="${PTREE:-${SDDIR}/portage/latest/portage-latest.tar.bz2}"
+	export PTREE="${PTREE:-${SDDIR}/portage/${LATEST}/portage-latest.tar.bz2}"
 	export RODIR="${RODIR:-${CADIR}/rootfs}"
 	export CKERN="${CKERN:-}"
 
@@ -78,8 +79,6 @@ prepare_portage() {
 
 	catalyst -v -c ${CCONF} -s $STAMP
 	cp -p /var/tmp/catalyst/snapshots/portage-latest.* ${SDDIR}/portage/${RELDA}
-	rm -f ${SDDIR}/portage/latest
-	ln -sf ${SDDIR}/portage/${RELDA} ${SDDIR}/portage/latest
 }
 
 sync_sources() {
@@ -130,15 +129,13 @@ build_seed_boot() {
 	#catalyst -v -f /home/catalyst/specs/amd64/hardened/stage4-nomultilib-minimal.spec -c ${CCONB} -C version_stamp=$STAMP
 
 	cp -p ${BDDIR}/stage*-amd64-latest.tar.bz2* ${SDDIR}/boot/${RELDA}
-	rm -f ${SDDIR}/boot/latest
-	ln -sf ${SDDIR}/boot/${RELDA} ${SDDIR}/boot/latest
 }
 
 build_seed_init() {
 	echo "### build_seed_init()"
 
 	clean_stage
-	cp ${SDDIR}/boot/latest/stage3-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/boot/${LATEST}/stage3-amd64-latest.tar.bz2* ${BDDIR}
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/stage1-nomultilib.spec -c ${CCONI} -C version_stamp=$STAMP
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/stage2-nomultilib.spec -c ${CCONI} -C version_stamp=$STAMP
@@ -146,18 +143,16 @@ build_seed_init() {
 	#catalyst -v -f /home/catalyst/specs/amd64/hardened/stage4-nomultilib-minimal.spec -c ${CCONI} -C version_stamp=$STAMP
 
 	cp -p ${BDDIR}/stage*-amd64-latest.tar.bz2* ${SDDIR}/init/${RELDA}
-	rm -f ${SDDIR}/init/latest
-	ln -sf ${SDDIR}/init/${RELDA} ${SDDIR}/init/latest
 }
 
 build_livecd_minimal() {
 	echo "### build_livecd_minimal()"
 
 	clean_stage
-	cp ${SDDIR}/init/latest/stage3-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/init/${LATEST}/stage3-amd64-latest.tar.bz2* ${BDDIR}
 	if [ "x${CKERN}" != "x" ] ; then
 		mkdir -p /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
-		cp -pR ${SDDIR}/kerncache/latest/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
+		cp -pR ${SDDIR}/kerncache/${LATEST}/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
 	fi
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/admincd-stage1-hardened-minimal.spec -c ${CCONF} -C version_stamp=$STAMP
@@ -165,24 +160,20 @@ build_livecd_minimal() {
 
 	cp -p ${BDDIR}/livecd-stage*-amd64-latest.tar.bz2* ${SDDIR}/minimal/${RELDA}
 	cp -p ${BDDIR}/amd64-latest.iso* ${SDDIR}/minimal/${RELDA}
-	rm -f ${SDDIR}/minimal/latest
-	ln -sf ${SDDIR}/minimal/${RELDA} ${SDDIR}/minimal/latest
 	[ ! -z "${PKDIR}" ] && rm -rf ${PKDIR}/*
 	mkdir -p ${PKDIR} ; cp -pr /var/tmp/catalyst/packages/hardened/livecd-stage1-amd64-latest/* ${PKDIR}
 
 	cp -pR /var/tmp/catalyst/kerncache/hardened/livecd-stage2-amd64-latest/*.bz2 ${SDDIR}/kerncache/${RELDA}
-	rm -f ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/kerncache/${RELDA} ${SDDIR}/kerncache/latest
 }
 
 build_livecd_admin() {
 	echo "### build_livecd_admin()"
 
 	clean_stage
-	cp ${SDDIR}/init/latest/stage3-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/init/${LATEST}/stage3-amd64-latest.tar.bz2* ${BDDIR}
 	if [ "x${CKERN}" != "x" ] ; then
 		mkdir -p /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
-		cp -pR ${SDDIR}/kerncache/latest/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
+		cp -pR ${SDDIR}/kerncache/${LATEST}/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
 	fi
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/admincd-stage1-hardened-admin.spec -c ${CCONF} -C version_stamp=$STAMP
@@ -190,24 +181,20 @@ build_livecd_admin() {
 
 	cp -p ${BDDIR}/livecd-stage*-amd64-latest.tar.bz2* ${SDDIR}/admin/${RELDA}
 	cp -p ${BDDIR}/amd64-latest.iso* ${SDDIR}/admin/${RELDA}
-	rm -f ${SDDIR}/admin/latest
-	ln -sf ${SDDIR}/admin/${RELDA} ${SDDIR}/admin/latest
 	[ ! -z "${PKDIR}" ] && rm -rf ${PKDIR}/*
 	mkdir -p ${PKDIR} ; cp -pr /var/tmp/catalyst/packages/hardened/livecd-stage1-amd64-latest/* ${PKDIR}
 
 	cp -pR /var/tmp/catalyst/kerncache/hardened/livecd-stage2-amd64-latest/*.bz2 ${SDDIR}/kerncache/${RELDA}
-	rm -f ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/kerncache/${RELDA} ${SDDIR}/kerncache/latest
 }
 
 build_livecd_desktop() {
 	echo "### build_livecd_desktop()"
 
 	clean_stage
-	cp ${SDDIR}/init/latest/stage3-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/init/${LATEST}/stage3-amd64-latest.tar.bz2* ${BDDIR}
 	if [ "x${CKERN}" != "x" ] ; then
 		mkdir -p /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
-		cp -pR ${SDDIR}/kerncache/latest/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
+		cp -pR ${SDDIR}/kerncache/${LATEST}/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
 	fi
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/admincd-stage1-hardened-desktop.spec -c ${CCONF} -C version_stamp=$STAMP
@@ -215,77 +202,61 @@ build_livecd_desktop() {
 
 	cp -p ${BDDIR}/livecd-stage*-amd64-latest.tar.bz2* ${SDDIR}/desktop/${RELDA}
 	cp -p ${BDDIR}/amd64-latest.iso* ${SDDIR}/desktop/${RELDA}
-	rm -f ${SDDIR}/desktop/latest
-	ln -sf ${SDDIR}/desktop/${RELDA} ${SDDIR}/desktop/latest
 	[ ! -z "${PKDIR}" ] && rm -rf ${PKDIR}/*
 	mkdir -p ${PKDIR} ; cp -pr /var/tmp/catalyst/packages/hardened/livecd-stage1-amd64-latest/* ${PKDIR}
 
 	cp -pR /var/tmp/catalyst/kerncache/hardened/livecd-stage2-amd64-latest/*.bz2 ${SDDIR}/kerncache/${RELDA}
-	rm -f ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/kerncache/${RELDA} ${SDDIR}/kerncache/latest
 }
 
 update_livecd_minimal() {
 	echo "### update_livecd_minimal()"
 
 	clean_stage
-	cp ${SDDIR}/minimal/latest/livecd-stage1-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/minimal/${LATEST}/livecd-stage1-amd64-latest.tar.bz2* ${BDDIR}
 	if [ "x${CKERN}" != "x" ] ; then
 		mkdir -p /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
-		cp -pR ${SDDIR}/kerncache/latest/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
+		cp -pR ${SDDIR}/kerncache/${LATEST}/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
 	fi
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/admincd-stage2-hardened-minimal.spec -c ${CCONF} -C version_stamp=$STAMP
 	cp -p ${BDDIR}/livecd-stage*-amd64-latest.tar.bz2* ${SDDIR}/minimal/${RELDA}
 	cp -p ${BDDIR}/amd64-latest.iso* ${SDDIR}/minimal/${RELDA}
-	rm -f ${SDDIR}/minimal/latest
-	ln -sf ${SDDIR}/minimal/${RELDA} ${SDDIR}/minimal/latest
 
 	cp -pR /var/tmp/catalyst/kerncache/hardened/livecd-stage2-amd64-latest/*.bz2 ${SDDIR}/kerncache/${RELDA}
-	rm -f ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/kerncache/${RELDA} ${SDDIR}/kerncache/latest
 }
 
 update_livecd_admin() {
 	echo "### update_livecd_admin()"
 
 	clean_stage
-	cp ${SDDIR}/admin/latest/livecd-stage1-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/admin/${LATEST}/livecd-stage1-amd64-latest.tar.bz2* ${BDDIR}
 	if [ "x${CKERN}" != "x" ] ; then
 		mkdir -p /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
-		cp -pR ${SDDIR}/kerncache/latest/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
+		cp -pR ${SDDIR}/kerncache/${LATEST}/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
 	fi
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/admincd-stage2-hardened-admin.spec -c ${CCONF} -C version_stamp=$STAMP
 	cp -p ${BDDIR}/livecd-stage*-amd64-latest.tar.bz2* ${SDDIR}/admin/${RELDA}
 	cp -p ${BDDIR}/amd64-latest.iso* ${SDDIR}/admin/${RELDA}
-	rm -f ${SDDIR}/admin/latest
-	ln -sf ${SDDIR}/admin/${RELDA} ${SDDIR}/admin/latest
 
 	cp -pR /var/tmp/catalyst/kerncache/hardened/livecd-stage2-amd64-latest/*.bz2 ${SDDIR}/kerncache/${RELDA}
-	rm -f ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/kerncache/${RELDA} ${SDDIR}/kerncache/latest
 }
 
 update_livecd_desktop() {
 	echo "### update_livecd_desktop()"
 
 	clean_stage
-	cp ${SDDIR}/desktop/latest/livecd-stage1-amd64-latest.tar.bz2* ${BDDIR}
+	cp ${SDDIR}/desktop/${LATEST}/livecd-stage1-amd64-latest.tar.bz2* ${BDDIR}
 	if [ "x${CKERN}" != "x" ] ; then
 		mkdir -p /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
-		cp -pR ${SDDIR}/kerncache/latest/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
+		cp -pR ${SDDIR}/kerncache/${LATEST}/*.bz2 /var/tmp/catalyst/kerncache/livecd-stage2-amd64-latest
 	fi
 
 	catalyst -v -f /home/catalyst/specs/amd64/hardened/admincd-stage2-hardened-desktop.spec -c ${CCONF} -C version_stamp=$STAMP source_subpath=hardened/livecd-stage1-amd64-latest.tar.bz2
 	cp -p ${BDDIR}/livecd-stage*-amd64-latest.tar.bz2* ${SDDIR}/desktop/${RELDA}
 	cp -p ${BDDIR}/amd64-latest.iso* ${SDDIR}/desktop/${RELDA}
-	rm -f ${SDDIR}/desktop/latest
 
 	cp -pR /var/tmp/catalyst/kerncache/hardened/livecd-stage2-amd64-latest/*.bz2 ${SDDIR}/kerncache/${RELDA}
-	rm -f ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/kerncache/${RELDA} ${SDDIR}/kerncache/latest
-	ln -sf ${SDDIR}/desktop/${RELDA} ${SDDIR}/desktop/latest
 }
 
 archive_digests() {
