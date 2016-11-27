f2b54e62188f0a60ced992a474bc0b55c4947e36
aggi <aggi@localhost>
2016-11-27 01:46:17 +0000

    relocated catalyst and genkernel share directories into etc/portage dir

---
+++ etc/portage/genkernel/defaults/config.sh
@@ -0,0 +1,35 @@
+#This file does not actually appear to be run at all anymore, and is kept as an example for new arches.
+# $Id: 6acf3bb624b8beac93c052d11f692b28111c4643 $
+#
+# Arch-specific options that normally shouldn't be changed.
+#
+KERNEL_MAKE_DIRECTIVE="bzImage"
+# since "" is allowed (this will translate in `make `, which is nice
+# for xen-based kernels, the default value of
+# KERNEL_MAKE_DIRECTIVE_OVERRIDE cannot be ""
+DEFAULT_KERNEL_MAKE_DIRECTIVE_OVERRIDE="--INVALID--"
+KERNEL_MAKE_DIRECTIVE_OVERRIDE="${DEFAULT_KERNEL_MAKE_DIRECTIVE_OVERRIDE}"
+KERNEL_MAKE_DIRECTIVE_2=""
+KERNEL_BINARY="arch/i386/boot/bzImage"
+KERNEL_BINARY_OVERRIDE=""
+
+DEFAULT_COMPRESS_INITRD=yes
+DEFAULT_COMPRESS_INITRD_TYPE=best
+
+#
+# Arch-specific defaults that can be overridden in the config file or on the
+# command line.
+#
+PORTAGE_MAKEOPTS="$(portageq envvar MAKEOPTS)"
+DEFAULT_MAKEOPTS="${PORTAGE_MAKEOPTS:- -j2}"
+
+DEFAULT_KERNEL_MAKE=make
+DEFAULT_UTILS_MAKE=make
+
+DEFAULT_KERNEL_CC=gcc
+DEFAULT_KERNEL_AS=as
+DEFAULT_KERNEL_LD=ld
+
+DEFAULT_UTILS_CC=gcc
+DEFAULT_UTILS_AS=as
+DEFAULT_UTILS_LD=ld
