1fb3fdc11764ffc808c25e6e62885743fe78221f
aggi <aggi@localhost>
2016-11-27 03:47:41 +0000

    minor change in autobuild

--- autobuild-all.sh
+++ autobuild-all.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -e
 # Copyright aggi 2016
 
-rm -f build.log
-
-#LATEST=20161117-1479426114
 PTREE=/home/seeds/gentoo/portage-hardened.tar
 
 source /home/catalyst/autobuild.sh
@@ -14,11 +11,12 @@ prepare_portage
 
 #build_seed_boot
 #build_seed_init
-#export PKDIR="/home/packages-minimal" ; rm -rf ${PKDIR}/*
-#build_livecd_minimal
-#export PKDIR="/home/packages-admin" ; rm -rf ${PKDIR}/*
-#build_livecd_admin
+export PKDIR="/home/packages-minimal" ; rm -rf ${PKDIR}/*
+build_livecd_minimal
+export PKDIR="/home/packages-admin" ; rm -rf ${PKDIR}/*
+build_livecd_admin
 export PKDIR="/home/packages-desktop" ; rm -rf ${PKDIR}/*
 build_livecd_desktop
 
 archive_digests
+
