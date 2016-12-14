0d8b39d9790d29a41fb1d8d4c4dd032bd6685a8d
aggi <aggi@localhost>
2016-12-14 05:49:47 +0000

    fetch and sync git repos

---
+++ autobuild-sources.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for i in data infra proj repo report sites ; do
+	cd /home/source/portage/${i}
+	for r in $(ls) ; do
+		echo /home/source/portage/${i}/${r}
+		cd /home/source/portage/${i}/${r}
+		git fsck
+		sg wanout -c "git pull --rebase"
+		cd -
+	done
+done
+
