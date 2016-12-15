9fed3055bbea575f30bc75c75eee556cf2a3281f
aggi <aggi@localhost>
2016-12-15 06:05:55 +0000

    fixed sshsrcport.sh helper script

--- rootfs/usr/local/bin/sshsrcport.sh
+++ rootfs/usr/local/bin/sshsrcport.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+
 DEST=$1
 DPRT=$2
 LPRT=$2
 SPRT=$3
 SUSR=$4
+
+if [ -z "${DEST}" -o -z "${DPRT}" -o -z "${SPRT}" ] ; then
+	echo "Usage: sshsrcport.sh <DEST> <DPRT> <SPRT> [SUSR]" ; exit 1
+fi
 
 #pass in log proto tcp from any port {SPRT} to port {DPRT}
 ncat -l ${LPRT} --sh-exec "ncat ${DEST} ${SPRT} -p ${DPRT}"
