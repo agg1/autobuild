0d5ec973c613343cfef7ba195fd0544e66565b3e
aggi <aggi@localhost>
2016-12-15 04:23:14 +0000

    added comments related to key bindings to irssi.sh

--- rootfs/usr/local/bin/irssi.sh
+++ rootfs/usr/local/bin/irssi.sh
@@ -5,6 +5,28 @@ export AWL_SSIZE=$(/bin/stty size)
 #T3=$(pgrep -u $USER -x irssi)
 T3=$(tmux list-sessions -F '#{session_name}' | grep main)
 
+#ctrl-b
+#1..N switch window
+#c  create window
+#w  list windows
+#n  next window
+#p  previous window
+#f  find window
+#,  name window
+#&  kill window
+#%  vertical split
+#"  horizontal split
+#o  swap panes
+#q  show pane numbers
+#x  kill pane
+#+  break pane into window (e.g. to select text by mouse to copy)
+#-  restore pane from window
+#⍽  space - toggle between layouts
+#<prefix> q (Show pane numbers, when the numbers show up type the key to goto that pane)
+#<prefix> { (Move the current pane left)
+#<prefix> } (Move the current pane right)
+#<prefix> z toggle pane zoom
+
 irssi_nickpane() {
 	tmux setw main-pane-width $(( $(tput cols) - 21));
 	tmux splitw -v "cat ~/.irssi/nicklistfifo";
