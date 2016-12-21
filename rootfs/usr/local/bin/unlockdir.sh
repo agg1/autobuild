d851a039bec7a7b5ca6fa3a70db9c72d5b9951d1
aggi <aggi@localhost>
2016-12-21 20:42:50 +0000

    removed sensitive scripts

--- rootfs/usr/local/bin/unlockdir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-CDIR=$1
-[ -z "${CDIR}" -o ! -e "${CDIR}" ] && echo "directory error" && exit
-/usr/sbin/e4crypt get_policy "${CDIR}" | grep "Error getting policy" > /dev/null 2>&1 && echo "policy error" && exit
-/usr/sbin/e4crypt add_key -S 0xffff $1
