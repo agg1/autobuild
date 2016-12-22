aed6caf06771df27644605f0b4baaf0e591b6b9a
aggi <aggi@localhost>
2016-12-22 11:13:30 +0000

    fixed mount option restrictions

--- rootfs/usr/local/bin/lockusrupdate.sh
+++ rootfs/usr/local/bin/lockusrupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
-mount -o remount,ro /etc 2> /dev/null
+mount -o remount,ro,nodev,nosuid,noexec /etc 2> /dev/null
+mount -o remount,nodev,nosuid,noexec /tmp 2> /dev/null
+mount -o remount,nodev,nosuid,noexec /var/tmp 2> /dev/null
+mount -o remount,nodev,nosuid,noexec /home 2> /dev/null
 mount -o remount,ro /usr 2> /dev/null
-mount -o remount,noexec /tmp 2> /dev/null
-mount -o remount,noexec /var/tmp 2> /dev/null
