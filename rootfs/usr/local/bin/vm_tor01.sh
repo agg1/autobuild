3b4c59cfe59eef8b2a41e8e73a6138c129faa207
aggi <aggi@localhost>
2016-12-25 17:00:07 +0000

    192M RAM

--- rootfs/usr/local/bin/vm_tor01.sh
+++ rootfs/usr/local/bin/vm_tor01.sh
@@ -2,7 +2,7 @@ export QEMU_AUDIO_DRV=alsa QEMU_AUDIO_TIMER_PERIOD=250
 
 VMNAME=tor01
 CPUNUM=1
-MEM=160M
+MEM=192M
 #DISKDRIVER="virtio"
 CDISO="-cdrom /home/virtual/tor01/tor01-latest.iso"
 OSDISK="-hda /home/virtual/tor01/tor01.cfg.img"
@@ -32,8 +32,10 @@ ${SOUNDHW} \
 -net none \
 -device nec-usb-xhci,bus=pci.0,id=ehci1 \
 -device nec-usb-xhci,bus=pci.0,id=ehci2 \
--device usb-host,vendorid=0x9710,productid=0x7830,id=usbeth1,bus=ehci1.0,port=1 \
+-device usb-host,hostbus=1,hostaddr=32,id=usbeth1,bus=ehci1.0,port=1 \
 ${FLOPPY} \
 ${CDISO} \
 ${OSDISK} \
 -boot order=cd,menu=on ${DAEMON} ${RUNAS}
+
+#-device usb-host,vendorid=0x9710,productid=0x7830,id=usbeth1,bus=ehci1.0,port=1 \
