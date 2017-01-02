a6106118be73eb75bb8adaae64304459d44441f0
aggi <aggi@localhost>
2017-01-02 22:19:14 +0000

    updated irssi and tmux main script

--- rootfs/usr/local/bin/irssi.sh
+++ rootfs/usr/local/bin/irssi.sh
@@ -5,28 +5,6 @@ export AWL_SSIZE=$(/bin/stty size)
 #T3=$(pgrep -u $USER -x irssi)
 T3=$(tmux list-sessions -F '#{session_name}' | grep irc)
 
-#ctrl-b
-#1..N switch window
-#c  create window
-#w  list windows
-#n  next window
-#p  previous window
-#f  find window
-#,  name window
-#&  kill window
-#%  vertical split
-#"  horizontal split
-#o  swap panes
-#q  show pane numbers
-#x  kill pane
-#+  break pane into window (e.g. to select text by mouse to copy)
-#-  restore pane from window
-#⍽  space - toggle between layouts
-#<prefix> q (Show pane numbers, when the numbers show up type the key to goto that pane)
-#<prefix> { (Move the current pane left)
-#<prefix> } (Move the current pane right)
-#<prefix> z toggle pane zoom
-
 irssi_nickpane() {
 	tmux setw main-pane-width $(( $(tput cols) - 21));
 	tmux splitw -v "cat ~/.irssi/nicklistfifo";
@@ -43,6 +21,7 @@ irssi_repair() {
 
 if [ -z "$T3" ]; then
 	tmux new-session -E -d -s irc
+	sleep 1
 	tmux new-window -t irc -n irssi sg wanout -c \
 	"systrace -d /usr/local/etc/systrace -ia /usr/bin/irssi -- --config ~/.config/irssi/config"
 #    irssi_nickpane
