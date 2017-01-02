626bbb67c42bab6b48f124a9ea99e1b345c50522
aggi <aggi@localhost>
2017-01-02 02:21:58 +0000

    log timesync to syslog

--- rootfs/usr/local/bin/timesync.sh
+++ rootfs/usr/local/bin/timesync.sh
@@ -3,7 +3,8 @@
 mount -o remount,rw /etc 2> /dev/null
 
 #ptbtime1.ptb.de
-sg wanout -c "ntpdate 192.53.103.108"
+TIMESYNC=$(sg wanout -c "ntpdate 192.53.103.108 2>&1" 2>&1)
+logger "TIMESYNC ${TIMESYNC}"
 hwclock --systohc
 
 #mount -o remount,ro /etc 2> /dev/null
