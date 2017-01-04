c6c9b6dc61154cbba38644184d64239fa3d66e18
aggi <aggi@localhost>
2017-01-04 14:07:03 +0000

    minor fix in tor launcher scripts

--- rootfs/usr/local/bin/tor.sh
+++ rootfs/usr/local/bin/tor.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p /var/log/tor 2>/dev/null
 chown -R tor:tor /var/log/tor 2>/dev/null
+chmod 700 /var/log/tor
 sg wanout -c "systrace -d /usr/local/etc/systrace -a /usr/bin/tor -- --defaults-torrc /usr/local/etc/tor/torrc "&
 
