4d6913e53b2fde17546b20aa0e324fc4b19c6f46
aggi <aggi@localhost>
2017-01-04 13:04:42 +0000

    fixed tor launcher to create data directory

--- rootfs/usr/local/bin/tor.sh
+++ rootfs/usr/local/bin/tor.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+mkdir -p /var/log/tor 2>/dev/null
+chown -R tor:tor /var/log/tor 2>/dev/null
 sg wanout -c "systrace -d /usr/local/etc/systrace -a /usr/bin/tor -- --defaults-torrc /usr/local/etc/tor/torrc "&
 
