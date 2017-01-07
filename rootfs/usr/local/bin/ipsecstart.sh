5d5750a7286da9c1e8de30b4b218bc2258e192b8
aggi <aggi@localhost>
2017-01-07 23:59:29 +0000

    ipsec helper scripts

---
+++ rootfs/usr/local/bin/ipsecstart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+REMOTEOIPS=$1
+if [ -z "${REMOTEIPS}" ] ;then
+        echo "no remote specified"
+        exit 1
+fi
+
+racoonctl -s /var/run/racoon.sock vd ${REMOTEIPS} 2>/dev/null
+sleep 2
+racoonctl -s /var/run/racoon.sock vc ${REMOTEIPS}
---
+++ rootfs/usr/local/bin/ipsecstop.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+REMOTEOIPS=$1
+if [ -z "${REMOTEIPS}" ] ;then
+        echo "no remote specified"
+        exit 1
+fi
+
+racoonctl -s /var/run/racoon.sock vd ${REMOTEIPS}
