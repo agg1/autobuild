6bc13feef7070922555fd4d9b38e942e073feeb1
aggi <aggi@localhost>
2017-01-13 01:46:07 +0000

    proxy01 build

---
+++ autobuild-vm_proxy01.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+# Copyright aggi 2016
+
+export LATEST="20161226-1482779549"
+export RELDA="proxy01-testing"
+export PTREE=/home/seeds/portage/${LATEST}/portage-latest.tar.bz2
+export CKERN="yes"
+export LVSIZ="128"
+export LVHOM="10"
+export LVLOG="70"
+
+source /home/catalyst/autobuild.sh
+prepare_system
+
+clean_portage
+prepare_portage
+umount /var/tmp/catalyst/builds || true
+
+export MACHINE="proxy01"
+export MACHID=44444444
+export PKDIR="/tmp/packages-${MACHINE}" ; rm -rf ${PKDIR}/*
+build_livecd_minimal_machine
+#cp -p /home/seeds/${MACHINE}/${RELDA}/* /home/virtual/${MACHINE}
