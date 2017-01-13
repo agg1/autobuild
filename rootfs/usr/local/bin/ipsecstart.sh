4e96cab698cf1158c2886c36b05beaa47f15975b
aggi <aggi@localhost>
2017-01-13 06:42:47 +0000

    do not force default gw with ipsecstart.sh

--- rootfs/usr/local/bin/ipsecstart.sh
+++ rootfs/usr/local/bin/ipsecstart.sh
@@ -6,12 +6,12 @@ if [ -z "${REMOTEIPS}" ] ;then
         exit 1
 fi
 
-gwif=`netstat --inet -rn|awk '($1 == "0.0.0.0"){print $8; exit}'`
-
-if [ -z "${gwif}" ] ; then
-	echo "no default gw for road warrior setup"
-	exit 1
-fi
+#gwif=`netstat --inet -rn|awk '($1 == "0.0.0.0"){print $8; exit}'`
+#
+#if [ -z "${gwif}" ] ; then
+#	echo "no default gw for road warrior setup"
+#	exit 1
+#fi
 
 racoonctl -s /var/run/racoon.sock vd ${REMOTEIPS} 2>/dev/null
 sleep 2
