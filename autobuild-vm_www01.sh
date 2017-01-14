be4cbc45552fb478f1db7271545ef135c19b511c
aggi <aggi@localhost>
2017-01-14 08:40:21 +0000

    www01 cfg and autobuild

---
+++ autobuild-vm_www01.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+# Copyright aggi 2016
+
+export LATEST="20161226-1482779549"
+export RELDA="www01-testing"
+export PTREE=/home/seeds/portage/${LATEST}/portage-latest.tar.bz2
+export CKERN="yes"
+export LVSIZ="128"
+export LVHOM="50"
+export LVLOG="50"
+
+source /home/catalyst/autobuild.sh
+prepare_system
+
+clean_portage
+prepare_portage
+umount /var/tmp/catalyst/builds || true
+
+export MACHINE="www01"
+export MACHID=44444445
+export PKDIR="/tmp/packages-${MACHINE}" ; rm -rf ${PKDIR}/*
+build_livecd_minimal_machine
+#cp -p /home/seeds/${MACHINE}/${RELDA}/* /home/virtual/${MACHINE}
