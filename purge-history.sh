64c3d2178c62ff946ffa93d7bf4595dd453018b9
Aggi <aggi@padwalker.com>
2017-02-04 20:28:47 +0000

    cleanup

--- purge-history.sh
+++ purge-history.sh
@@ -1,3 +1,5 @@
-git filter-branch --index-filter 'git rm -r --cached --ignore-unmatch cfg'
-git filter-branch --index-filter 'git rm -r --cached --ignore-unmatch cscripts'
+git filter-branch -f --index-filter 'git rm -r --cached --ignore-unmatch cfg'
+git update-ref -d refs/original/refs/heads/master
+git filter-branch -f --index-filter 'git rm -r --cached --ignore-unmatch cscripts'
+git update-ref -d refs/original/refs/heads/master
 
