67983ba391e7913b2423efa4fd27f35b8beb2afd
Aggi <aggi@padwalker.com>
2017-02-04 13:42:26 +0000

    updated conversion script for odt to html to include author and title markdown meta

--- rootfs/usr/local/bin/convert-odt2html.sh
+++ rootfs/usr/local/bin/convert-odt2html.sh
@@ -2,6 +2,7 @@
 
 CONFIG=/path/to/tidy_options.conf
 CONVDIR=$1
+AUTHOR=$2
 
 if [ -z "${CONVDIR}" -o ! -x "${CONVDIR}" ] ; then
 	echo "dir error" ; exit 1
@@ -22,8 +23,14 @@ do
 	basename=$(basename "$F")
 	filename=$(basename "$basename" .odt)
 	dirname=$(dirname "$F")
-	/usr/bin/soffice --headless --convert-to pdf --outdir $dirname "$F"
 	/usr/bin/soffice --headless --convert-to html --outdir $dirname "$F"
-	/usr/bin/tidy -config /usr/local/etc/convert-odt2html.conf -o "${dirname}/${filename}.xhtml" "${dirname}/${filename}.html"
-	mv "${dirname}/${filename}.xhtml" "${dirname}/${filename}.html"
+	/usr/bin/soffice --headless --convert-to pdf --outdir $dirname "$F"
+	#/usr/bin/tidy -config /usr/local/etc/convert-odt2html.conf -o "${dirname}/${filename}.xhtml" "${dirname}/${filename}.html"
+	#mv "${dirname}/${filename}.xhtml" "${dirname}/${filename}.html"
+	#rm -f ${dirname}/${filename}.xhtml
+	cat "${dirname}/${filename}.html" | sed 's/<title>/<!-- [[!meta title=\"/' | sed 's/<\/title>/\"]] -->/' > "${dirname}/${filename}.html.tmp"
+	mv "${dirname}/${filename}.html.tmp" "${dirname}/${filename}.html"
+	if [ ! -z "${AUTHOR}" ] ;then
+		echo "<!-- [[!meta author=\"${AUTHOR}\"]] -->" >> "${dirname}/${filename}.html"
+	fi
 done
