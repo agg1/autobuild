8c65053fc425c4d60c3e65ce6e84f044a877ede4
Aggi <aggi@padwalker.com>
2017-02-05 00:53:24 +0000

    minor changes

--- git-remotes.sh
+++ git-remotes.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 git remote add github https://github.com/agg1/catalyst.git
-git push -u github master
+git push github master
 git remote add origin ssh://www02/home/testing/catalyst.git
+git push -u origin master
+
