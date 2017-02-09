407f488ccb810fe353c5b79b4ddcc20707f03b2c
Aggi <aggi@padwalker.com>
2017-02-09 21:25:03 +0000

    fix critical bug in writable unlocking script

--- rootfs/usr/local/bin/writable.sh
+++ rootfs/usr/local/bin/writable.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-WDIR=$1
-[ -z "${WDIR}" -o ! -e "${WDIR}" ] && echo "directory error" && exit
+TDIR=$1
+[ -z "${TDIR}" -o ! -e "${TDIR}" ] && echo "directory error" && exit
+
+WDIR="$(realpath ${TDIR})"
 
 echo "### writable()"
 if [ -e ${WDIR}/.writeable ] ; then
@@ -9,6 +11,7 @@ else
 	modprobe overlay || true
 	rm -rf /home/livecd_overlay/upper/${WDIR} /home/livecd_overlay/work/${WDIR}
 	mkdir -p /home/livecd_overlay/upper/${WDIR} /home/livecd_overlay/work/${WDIR}
-	mount -t overlay overlay -o rw,dirsync,sync,lowerdir=${WDIR},upperdir=/home/livecd_overlay/upper/${WDIR},workdir=/home/livecd_overlay/work/ ${WDIR}
+	mount -t overlay overlay -o \
+	rw,dirsync,sync,lowerdir=${WDIR},upperdir=/home/livecd_overlay/upper/${WDIR},workdir=/home/livecd_overlay/work/${WDIR} ${WDIR}
 	touch ${WDIR}/.writeable
 fi
