0a91c10ce607e51afba84cf4db4af9e02395fea3
Aggi <aggi@padwalker.com>
2017-02-20 21:51:02 +0000

    git-fsck with source fetching

--- autobuild-sources.sh
+++ autobuild-sources.sh
@@ -5,9 +5,10 @@ umask 022
 for i in data infra proj repo report sites ; do
 	cd /home/source/portage/${i}
 	for r in $(ls) ; do
-		echo /home/source/portage/${i}/${r}
+		[ ! -e /home/source/portage/${i}/${r}/.git ] && continue
+		echo "syncing /home/source/portage/${i}/${r}"
 		cd /home/source/portage/${i}/${r}
-		git fsck
+		git reset --hard ; git clean -f ; git fsck
 		sg wanout -c "git pull --rebase"
 		cd -
 	done
