eb34dcb5153c85d2d68085ef54ce9e82e723cf4c
Aggi <aggi@padwalker.com>
2017-02-21 07:41:29 +0000

    use cache with minimal-testing build

--- autobuild-minimal.sh
+++ autobuild-minimal.sh
@@ -2,8 +2,8 @@
 # Copyright aggi 2016
 export LATEST=minimal-testing
 export RELDA=minimal-testing
-#export CLEAN=false
-#CKERN="yes"
+export CLEAN=false
+CKERN="yes"
 
 #cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
 #cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
@@ -18,10 +18,6 @@ prepare_portage
 #build_seed_init
 export PKDIR="/home/packages/minimal/${RELDA}"
 build_livecd_minimal
-#export PKDIR="/home/packages-admin" ; rm -rf ${PKDIR}/*
-#build_livecd_admin
-#export PKDIR="/home/packages-desktop" ; rm -rf ${PKDIR}/*
-#build_livecd_desktop
 
 #archive_digests
 #commit_seed minimal
