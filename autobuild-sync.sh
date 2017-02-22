7217c7a29f00e364f974ba622773b9307096d6f4
Aggi <aggi@padwalker.com>
2017-02-22 14:51:40 +0000

    sync helper script

--- autobuild-sync.sh
+++ autobuild-sync.sh
@@ -5,9 +5,22 @@
 rsync -av /home/distfiles/ /media/backup/distfiles/
 rsync -av /media/backup/distfiles/ /home/distfiles/
 
-for d in autobuild seeds extra_overlay portage ; do
-	cd $HOME/${d}
+cd /home/seeds
+git pull --rebase origin master
+git push --tags origin master
+
+for d in autobuild extra_overlay portage ; do
+	cd /home/${d}
 	git fsck
 	git pull --rebase origin master
 	git push --tags origin master
+done
+
+for d in autobuild extra_overlay portage ; do
+	cd /home/${d}
+	sg lanout -c "git push --tags www02 master || true" || true
+done
+for d in autobuild extra_overlay portage ; do
+	cd /home/${d}
+	sg wanout -c "git push --tags github master || true" || true
 done
