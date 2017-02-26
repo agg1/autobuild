7918f4b34c0e924ccbece7060a688cc34e01c4b4
Aggi <aggi@padwalker.com>
2017-02-26 23:37:02 +0000

    sign all flavors

--- autobuild-sign.sh
+++ autobuild-sign.sh
@@ -5,12 +5,22 @@ RELDA=$1
 
 LOGDATE="$(date +%Y%m%d) "
 LOGDEV="/dev/usb/lp0"
-gitsigtaglog.sh /home/autobuild		${RELDA} /home/autolog/${RELDA}-autobuild.log
-gitsigtaglog.sh /home/seeds		${RELDA} /home/autolog/${RELDA}-seeds.log
-gitsigtaglog.sh /home/portage		${RELDA} /home/autolog/${RELDA}-portage.log
-gitsigtaglog.sh /home/extra_overlay	${RELDA} /home/autolog/${RELDA}-extra_overlay.log
-lprlog.sh -f /home/autobuild/digests.log 		-p "${LOGDATE} digests:"	-d ${LOGDEV}
-lprlog.sh -f /home/autolog/${RELDA}-autobuild.log	-p "${LOGDATE} autobuild:"	-d ${LOGDEV}
-lprlog.sh -f /home/autolog/${RELDA}-seeds.log		-p "${LOGDATE} seeds:"		-d ${LOGDEV}
-lprlog.sh -f /home/autolog/${RELDA}-portage.log		-p "${LOGDATE} portage:"	-d ${LOGDEV}
-lprlog.sh -f /home/autolog/${RELDA}-extra_overlay.log	-p "${LOGDATE} extra_overlay:"	-d ${LOGDEV}
+gitsigtaglog.sh -r /home/autobuild	-t ${RELDA} -l /home/autolog/autobuild.log
+gitsigtaglog.sh -r /home/seeds		-t ${RELDA} -l /home/autolog/seeds.log
+gitsigtaglog.sh -r /home/portage	-t ${RELDA} -l /home/autolog/portage.log
+gitsigtaglog.sh -r /home/extra_overlay	-t ${RELDA} -l /home/autolog/extra_overlay.log
+cp -p /home/autobuild/digests.log /home/autolog/digests.log
+lprlog.sh -f /home/autolog/autobuild.log	-p "${LOGDATE} autobuild:"	-d ${LOGDEV} -n
+lprlog.sh -f /home/autolog/seeds.log		-p "${LOGDATE} seeds:"		-d ${LOGDEV} -n
+lprlog.sh -f /home/autolog/portage.log		-p "${LOGDATE} portage:"	-d ${LOGDEV} -n
+lprlog.sh -f /home/autolog/extra_overlay.log	-p "${LOGDATE} extra_overlay:"	-d ${LOGDEV} -n
+lprlog.sh -f /home/autobuild/digests.log 	-p "${LOGDATE} digests:"	-d ${LOGDEV} -n
+
+cd /home/autolog
+git add digests.log
+git add autobuild.log
+git add seeds.log
+git add portage.log
+git add extra_overlay.log
+git commit -m "${RELDA}" .
+git push origin master
