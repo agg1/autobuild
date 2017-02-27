d3d60aab2ec6ad1f00402c505b94ffc48d0c01fc
Autobuild <autobuild@padwalker.com>
2017-02-27 06:15:22 +0000

    cleanup etc_overlay before remounting

--- rootfs/usr/local/bin/locketc.sh
+++ rootfs/usr/local/bin/locketc.sh
@@ -8,7 +8,7 @@ else
 	umount /etc 2>/dev/null
 	touch /etc/.readonly
 	modprobe overlay || true
-	rm -rf /tmp/etc_overlay/etc
+	rm -rf /tmp/etc_overlay/
 	mkdir -p /tmp/etc_overlay/upper/etc /tmp/etc_overlay/work/etc
 	mount -t overlay overlay -o ro,noexec,nodev,nosuid,dirsync,sync,lowerdir=/etc,upperdir=/tmp/etc_overlay/upper/etc,workdir=/tmp/etc_overlay/work/etc /etc
 fi
