d86253c89bacb479a3269a8d3fdf5f3db1125179
Autobuild <autobuild@padwalker.com>
2017-03-01 21:10:18 +0000

    sync repositories on build host

--- autobuild-pull.sh
+++ autobuild-pull.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -e
-# Copyright aggi 2016
-
+# Copyright aggi 2017
+cd /home/autolog ; git pull --ff-only origin master
+cd /home/autobuild ; git pull --ff-only origin master
+cd /home/extra_overlay ; git pull --ff-only origin master
+cd /home/seeds ; git pull --ff-only origin master
--- autobuild-push.sh
+++ autobuild-push.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -e
-# Copyright aggi 2016
-
+# Copyright aggi 2017
+cd /home/autolog ; git push --tags origin master
+cd /home/autobuild ; git push --tags origin master
+cd /home/extra_overlay ; git push --tags origin master
+cd /home/seeds ; git push --tags origin master
