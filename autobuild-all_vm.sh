cc26a6296a6bedee1fd7c95ee994d8be589bcdf1
Autobuild <autobuild@padwalker.com>
2017-03-02 01:25:46 +0000

    unlock repos and clean

--- autobuild-all_vm.sh
+++ autobuild-all_vm.sh
@@ -6,6 +6,11 @@ export RELDA="${LATEST}"
 export CKERN=yes
 [ -z "${LATEST}" ] && echo "missing latest" && exit 1
 
+cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
+git clean -df .
+cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
+git clean -df .
+
 for vm in fw01 irc01 proxy01 tor01 www01 ; do
 	/bin/sh autobuild-vm_${vm}.sh
 done
