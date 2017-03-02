ceb71726585df6519c8862968f50125793dc40f6
Autobuild <autobuild@padwalker.com>
2017-03-02 01:26:06 +0000

    git clean and commit seeds

--- autobuild-update.sh
+++ autobuild-update.sh
@@ -7,7 +7,9 @@ export CKERN=true
 [ -z "${LATEST}" ] && echo "LATEST not set" && exit 1
 
 cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
+git clean -df .
 cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
+git clean -df .
 
 source /home/autobuild/autobuild.sh
 prepare_system
@@ -26,5 +28,5 @@ archive_digests
 commit_seed minimal
 commit_seed admin
 commit_seed desktop
-commit_seed portage
 commit_seed kerncache
+commit_seed portage
