003580a35ef08315543228d06b00d7c23e947dca
Autobuild <autobuild@padwalker.com>
2017-03-04 07:14:47 +0000

    unlock git before pull

--- autobuild-pull.sh
+++ autobuild-pull.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 # Copyright aggi 2017
+/usr/local/bin/unlockdir.sh /media/backup/git
 cd /home/autolog ; git pull --ff-only origin master
 cd /home/autobuild ; git pull --ff-only origin master
 cd /home/extra_overlay ; git pull --ff-only origin master
