064789f173f87801d2781ebf50063e31a241dd53
Aggi <aggi@padwalker.com>
2017-03-05 03:58:11 +0000

    vm startup and stop scripts

---
+++ rootfs/usr/local/bin/vm_stop.sh
@@ -0,0 +1,36 @@
+#!/bin/sh - 
+#===============================================================================
+#
+#          FILE: vm_stop.sh
+# 
+#         USAGE: ./vm_stop.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: -- (), 
+#  ORGANIZATION: 
+#       CREATED: 03/05/17 02:58
+#      REVISION:  ---
+#===============================================================================
+
+VMNAME="$1"
+
+[ -z "${VMNAME}" ] && echo "machine name missing" && exit 1
+
+POWEROFF=""
+TRYCOUNT=0
+while [ "x${POWEROFF}" = "x" ] ; do
+	TRYCOUNT=$(($TRYCOUNT+1))
+	if [ $TRYCOUNT -gt 20 ]; then
+		echo "failed system_powerdown for ${VMNAME} ... killing"
+		kill -9 $(cat /var/run/qemu-${VMNAME}.pid) 2>/dev/null
+		sleep 5
+		exit
+	fi
+	echo system_powerdown | ncat -U /root/qemu-monitor-${VMNAME} 2>/dev/null && sleep 10 || POWEROFF="true"
+done
+
