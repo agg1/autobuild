ec869315e05aa47163986b345a8eba291dd0eff2
Aggi <aggi@padwalker.com>
2017-03-11 02:40:39 +0000

    package build stage

--- autobuild-packages.sh
+++ autobuild-packages.sh
@@ -6,10 +6,10 @@ export LATEST=$1
 export CKERN=true
 [ -z "${LATEST}" ] && echo "LATEST not set" && exit 1
 
-#cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
-#git clean -df .
-#cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
-#git clean -df .
+cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
+git clean -df .
+cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
+git clean -df .
 
 source /home/autobuild/autobuild.sh
 prepare_system
