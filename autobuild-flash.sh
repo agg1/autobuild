737079bdd81f9ef4cdae0915dbc98138742e44d3
Aggi <aggi@padwalker.com>
2017-03-22 18:27:58 +0000

    bugfix

--- autobuild-flash.sh
+++ autobuild-flash.sh
@@ -6,10 +6,10 @@ export CLEAN=true
 export CKERN=false
 [ -z "${LATEST}" ] && echo "LATEST not set" && exit 1
 
+#git clean -df .
 #cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
 #git clean -df .
 #cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
-#git clean -df .
 
 source /home/autobuild/autobuild.sh
 prepare_system
--- autobuild-update.sh
+++ autobuild-update.sh
@@ -7,10 +7,10 @@ export CKERN=true
 #export LATEST=20161126-1480193160
 [ -z "${LATEST}" ] && echo "LATEST not set" && exit 1
 
+git clean -df .
 cd /home/autobuild; git crypt unlock /media/backup/git/catalyst.gcr; cd -
 git clean -df .
 cd /home/extra_overlay; git crypt unlock /media/backup/git/catalyst.gcr; cd -
-git clean -df .
 
 source /home/autobuild/autobuild.sh
 prepare_system
