5d6c164ae45c9e8d7566664f48a17fbbc8d1dbe8
aggi <aggi@padwalker.com>
2018-03-30 19:49:24 +0000

    removed deprecated scripts

--- prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-umount /media/backup 2>/dev/null
-mount /dev/mapper/backup /media/backup
-unlockdir.sh /media/backup/git/
-cd /home/extra_overlay ; git crypt unlock /media/backup/git/catalyst.gcr
-cd /home/autobuild ; git crypt unlock /media/backup/git/catalyst.gcr
