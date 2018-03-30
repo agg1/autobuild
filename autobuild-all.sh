add65c8269e7d480a3bef4548ba4f6801a5f0a8b
aggi <aggi@padwalker.com>
2018-03-30 17:13:18 +0000

    final autobuild-all.sh

--- autobuild-all.sh
+++ autobuild-all.sh
@@ -1,45 +1,40 @@
 #!/bin/sh -e
-# Copyright aggi 2016
+# Copyright aggi 2016,2017,2018
 
-cd /home/autobuild ; git clean -df .
-git crypt unlock /media/backup/git/catalyst.gcr; cd -
-cd /home/extra_overlay ; git clean -df .
-git crypt unlock /media/backup/git/catalyst.gcr; cd -
-
-#if [ -f /tmp/.relda ]; then
-#	export RELDA=$(cat /tmp/.relda)
-#fi
+if [ -f /tmp/.relda ]; then
+	export RELDA=$(cat /tmp/.relda)
+	export NOCLEAN="true"
+else
+	:> /home/autolog/build.log
+fi
 
 source /home/autobuild/autobuild.sh
-prepare_system
 
+prepare_system
 clean_portage
 prepare_portage
 
-#sync_portage
-##fetch_portage
-#fetch_wget
-#fetch_catalyst
+sync_portage
+#fetch_portage
+fetch_wget
+fetch_catalyst
 
 build_seed_boot
 build_seed_init
+
 export PKDIR="/home/packages/minimal/${RELDA}"
 rm -rf /home/packages/minimal/*
-build_livecd_minimal
+build_livecd_minimal 2>&1 | tee -a /home/autolog/build.log
+
 export PKDIR="/home/packages/admin/${RELDA}"
 rm -rf /home/packages/admin/*
-build_livecd_admin
+build_livecd_admin  2>&1 | tee -a /home/autolog/build.log
+
 export PKDIR="/home/packages/desktop/${RELDA}"
 rm -rf /home/packages/desktop/*
-build_livecd_desktop
-export PKDIR="/home/packages/desktop/${RELDA}"
-#build_livecd_packages
+build_livecd_desktop 2>&1 | tee -a /home/autolog/build.log
 
-archive_digests
-commit_seed boot
-commit_seed init
-commit_seed minimal
-commit_seed admin
-commit_seed desktop
-commit_seed portage
-commit_seed kerncache
+export PKDIR="/home/packages/desktop/${RELDA}"
+build_livecd_packages
+
+sign_release
