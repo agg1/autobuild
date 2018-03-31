82125ca223d927d7665788819758e86b917bdf13
aggi <aggi@padwalker.com>
2018-03-31 14:33:13 +0000

    autobuild-update.sh

--- autobuild-update.sh
+++ autobuild-update.sh
@@ -2,7 +2,7 @@
 # Copyright aggi 2016,2017,2018
 
 export LATEST=$1
-#export CKERN=true
+export CKERN=true
 #export LATEST=20161126-1480193160
 [ -z "${LATEST}" ] && echo "LATEST not set" && exit 1
 
@@ -14,10 +14,7 @@ else
 fi
 
 source /home/autobuild/autobuild.sh
-
 prepare_system
-clean_portage
-prepare_portage
 
 export PKDIR="/home/packages/minimal/${LATEST}"
 update_livecd_minimal 2>&1 | tee -a /home/autolog/build.log
