0f2f0a6dae830a1057f3cc8609cb087d2f93862a
aggi <aggi@padwalker.com>
2018-04-18 19:19:58 +0000

    cleanups

--- git-remotes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git remote add origin /media/backup/git/autobuild.git
-git push -u origin master
-
-git remote add github https://github.com/agg1/autobuild.git
-git push github master
-
-git remote add www02 ssh://www02/home/testing/autobuild.git
-git push www02 master
