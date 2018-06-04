175b9894f08254cc0e1ca995694268f93813ce1e
autobuild <autobuild@padwalker.com>
2018-06-04 08:56:28 +0000

    init stage3 tarball for multilib modified to avoid conflichts

--- stage-libressl.sh
+++ stage-libressl.sh
@@ -5,11 +5,12 @@ cd /tmp/stage3
 #tar -xf ../stage3-amd64-hardened+nomultilib-20160908.tar.bz2
 tar -xf ../stage3-amd64-hardened-20180531T214503Z.tar.xz
 
-mkdir -p /tmp/stage3/usr/portage/distfiles
+mkdir -p /tmp/stage3/usr/portage/
 
 mount --bind /dev /tmp/stage3/dev
 mount --bind /proc /tmp/stage3/proc
 mount --bind /usr/portage /tmp/stage3/usr/portage
+mkdir -p /tmp/stage3/usr/portage/distfiles
 mount --bind /usr/distfiles /tmp/stage3/usr/portage/distfiles
 
 chroot /tmp/stage3
@@ -27,13 +28,38 @@ echo "dev-lang/python ~amd64" >> /etc/portage/package.accept_keywords
 #echo "dev-lang/python-exec ~amd64" >> /etc/portage/package.accept_keywords
 #echo "dev-lang/eselect-python ~amd64" >> /etc/portage/package.accept_keywords
 
+#
+echo "dev-lang/perl ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Test-Harness ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-ExtUtils-ParseXS ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Getopt-Long ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-version ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Test-Harness ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-ExtUtils-MakeMaker ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Perl-OSType ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Data-Dumper ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Module-Metadata ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-File-Spec ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-CPAN-Meta ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-Parse-CPAN-Meta ~amd64" >> /etc/portage/package.accept_keywords
+echo "virtual/perl-JSON-PP ~amd64" >> /etc/portage/package.accept_keywords
+#
+echo "sys-libs/gdbm ~amd64" >> /etc/portage/package.accept_keywords
+
+emerge -1q gentoolkit && \
 emerge -C openssl && \
 emerge -1q libressl && \
 emerge -1q openssh && \
 emerge -1q wget && \
+emerge -1q =sys-libs/gdbm-1.14.1 && \
 emerge -1q =dev-lang/python-2.7.14-r2 =dev-lang/python-3.5.5-r1 && \
 emerge -1q net-misc/iputils && \
-emerge -q @preserved-rebuild
+emerge --update --deep dev-lang/perl && \
+emerge -q @preserved-rebuild && \
+revdep-rebuild && \
+emerge -C gentoolkit
+
+# gdbm and perl,libintl-perl emerge required otherwise boot seed stage build fails with conflict
 
 exit
 
@@ -41,6 +67,7 @@ umount /tmp/stage3/usr/portage/distfiles
 umount /tmp/stage3/usr/portage
 umount /tmp/stage3/proc
 umount /tmp/stage3/dev
+rmdir /usr/portage/distfiles
 
 cd /tmp/stage3/
 tar -jcpf ../stage3-amd64-hardened+multilib-libressl.tar.bz2 .
