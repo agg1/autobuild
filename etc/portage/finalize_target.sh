82aa946ed9cf0379b54eeed37bc90457227bf7db
autobuild <autobuild@padwalker.com>
2018-06-11 21:51:01 +0000

    ensure var/log/{archive,audit} directories exist

--- etc/portage/finalize_target.sh
+++ etc/portage/finalize_target.sh
@@ -2,3 +2,7 @@
 
 find /usr/lib/ -name '*.pyc' -delete
 find /usr/lib/ -name '*.pyo' -delete
+mkdir -p /var/log/audit
+mkdir -p /var/log/archive
+chmod 700 /var/log/audit
+chmod 700 /var/log/archive
