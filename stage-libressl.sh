4a2ee933c053d4d9a28a9723785bcc34ffb9dec4
autobuild <autobuild@padwalker.com>
2018-12-13 08:18:19 +0000

    updated libressl staging for uclibc-hardened build

--- stage-libressl.sh
+++ stage-libressl.sh
@@ -1,9 +1,12 @@
 # HOWTO libressl
 
+cd /
+rm -rf /tmp/stage3
 mkdir -p /tmp/stage3
 cd /tmp/stage3
 #tar -xf ../stage3-amd64-hardened+nomultilib-20160908.tar.bz2
-tar -xf ../stage3-amd64-hardened-20180531T214503Z.tar.xz
+#tar -xf ../stage3-amd64-hardened-20180531T214503Z.tar.xz
+tar -xf /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-20181105.tar.bz2
 
 mkdir -p /tmp/stage3/usr/portage/
 
@@ -15,10 +18,11 @@ mount --bind /usr/distfiles /tmp/stage3/usr/portage/distfiles
 
 chroot /tmp/stage3
 
-echo 'USE="${USE} ssl openssl libressl curl_ssl_libressl -gcrypt -gnutls -nettle"' >> /etc/portage/make.conf
-echo '*/* ssl openssl libressl curl_ssl_libressl -gcrypt -gnutls -nettle' > /etc/portage/package.use/libressl
-echo 'net-misc/wget ssl openssl libressl curl_ssl_libressl -gcrypt -gnutls -nettle' >> /etc/portage/package.use/libressl
-echo 'net-misc/iputils ssl openssl libressl curl_ssl_libressl -gcrypt -gnutls -nettle -caps -filecaps' > /etc/portage/package.use/iputils 
+echo 'USE="${USE} ssl uclibc libressl -openssl curl_ssl_libressl -curl_ssl_openssl -gcrypt -gnutls -nettle"' >> /etc/portage/make.conf
+echo 'USE_ORDER="pkg:env:conf:defaults:pkginternal:repo:env.d"' >> /etc/portage/make.conf
+echo 'net-misc/curl ssl libressl curl_ssl_libressl -curl_ssl_openssl -gcrypt -gnutls -nettle' > /etc/portage/package.use/libressl
+echo 'net-misc/wget ssl libressl -gcrypt -gnutls -nettle' >> /etc/portage/package.use/libressl
+echo 'net-misc/iputils ssl libressl gcrypt -gnutls -nettle -caps -filecaps' > /etc/portage/package.use/iputils 
 
 #echo "dev-libs/libressl ~amd64" >> /etc/portage/package.accept_keywords
 #echo "net-misc/openssh ~amd64" >> /etc/portage/package.accept_keywords 
@@ -28,38 +32,33 @@ echo "dev-lang/python ~amd64" >> /etc/portage/package.accept_keywords
 #echo "dev-lang/python-exec ~amd64" >> /etc/portage/package.accept_keywords
 #echo "dev-lang/eselect-python ~amd64" >> /etc/portage/package.accept_keywords
 
-#
-echo "dev-lang/perl ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Test-Harness ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-ExtUtils-ParseXS ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Getopt-Long ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-version ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Test-Harness ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-ExtUtils-MakeMaker ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Perl-OSType ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Data-Dumper ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Module-Metadata ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-File-Spec ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-CPAN-Meta ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-Parse-CPAN-Meta ~amd64" >> /etc/portage/package.accept_keywords
-echo "virtual/perl-JSON-PP ~amd64" >> /etc/portage/package.accept_keywords
-#
+#echo "dev-lang/perl ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Test-Harness ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-ExtUtils-ParseXS ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Getopt-Long ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-version ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Test-Harness ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-ExtUtils-MakeMaker ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Perl-OSType ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Data-Dumper ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Module-Metadata ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-File-Spec ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-CPAN-Meta ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-Parse-CPAN-Meta ~amd64" >> /etc/portage/package.accept_keywords
+#echo "virtual/perl-JSON-PP ~amd64" >> /etc/portage/package.accept_keywords
 echo "sys-libs/gdbm ~amd64" >> /etc/portage/package.accept_keywords
 
-# file collision with pyblake and certifi package, who knows, hotfix
-rm -f /usr/lib64/python2.7/site-packages/pyblake2.so /usr/lib64/python3.5/site-packages/pyblake2.cpython-35m-x86_64-linux-gnu.so
-
 emerge -C openssl && \
+emerge -C curl && \
 emerge -1q libressl && \
 emerge -1q openssh && \
 emerge -1q wget && \
 emerge -1q =sys-libs/gdbm-1.14.1 && \
-emerge -1q =dev-lang/python-2.7.14-r2 =dev-lang/python-3.5.5-r1 && \
+emerge -1q =dev-lang/python-2.7.14-r2 =dev-lang/python-3.6.5-r1 && \
 emerge -1q net-misc/iputils && \
+emerge -1q net-misc/curl && \
 emerge --update --deep dev-lang/perl && \
 emerge -q @preserved-rebuild
-
-# gdbm and perl,libintl-perl emerge required otherwise boot seed stage build fails with conflict
 
 exit
 
@@ -67,11 +66,15 @@ umount /tmp/stage3/usr/portage/distfiles
 umount /tmp/stage3/usr/portage
 umount /tmp/stage3/proc
 umount /tmp/stage3/dev
-rmdir /usr/portage/distfiles
 
 cd /tmp/stage3/
-tar -jcpf ../stage3-amd64-hardened+multilib-libressl.tar.bz2 .
-cd /tmp
-shash -a md5 stage3-amd64-hardened+multilib-libressl.tar.bz2 > stage3-amd64-hardened+multilib-libressl.tar.bz2.DIGESTS
-shash -a sha256 stage3-amd64-hardened+multilib-libressl.tar.bz2 >> stage3-amd64-hardened+multilib-libressl.tar.bz2.DIGESTS
-shash -a whirlpool stage3-amd64-hardened+multilib-libressl.tar.bz2 >> stage3-amd64-hardened+multilib-libressl.tar.bz2.DIGESTS
+mount -o remount,rw /media/DISTFILES
+
+tar -jcpf /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2 .
+shash -a md5 /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2 > /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2.DIGESTS
+shash -a sha256 /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2 >> /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2.DIGESTS
+shash -a whirlpool /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2 >> /media/DISTFILES/release/release-2018/gentoo/stage3-amd64-uclibc-hardened-libressl-nolocale-20181213.tar.bz2.DIGESTS
+
+mount -o remount,ro /media/DISTFILES
+cd /
+rm -rf /tmp/stage3
