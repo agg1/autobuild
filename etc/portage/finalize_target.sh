fe10cec17229e0c88f0b4a4c7e1f5ff5272508e7
autobuild <autobuild@padwalker.com>
2018-12-25 07:06:59 +0000

    cosmetic fix

--- etc/portage/finalize_target.sh
+++ etc/portage/finalize_target.sh
@@ -6,8 +6,8 @@ mkdir -p /var/log/audit
 mkdir -p /var/log/archive
 chmod 700 /var/log/audit
 chmod 700 /var/log/archive
-cat /tmp/.reldate > /etc/BUILDDATE
-uname -a >> /etc/BUILDHOST
+#cat /tmp/.reldate > /etc/BUILDDATE
+uname -a > /etc/BUILDHOST
 rm -rf /etc/portage/rootfs
 rm -rf "/usr/share/applications/links_-g_%u-links-2.desktop"
 rm -rf /etc/portage/patches
