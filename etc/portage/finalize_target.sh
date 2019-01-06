76b0ceb74f4aabfc9003e96d4dd979abb3c6c296
autobuild <autobuild@padwalker.com>
2019-01-06 07:53:11 +0000

    cleanup added to finalize_target.sh

--- etc/portage/finalize_target.sh
+++ etc/portage/finalize_target.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+uname -a > /etc/BUILDHOST
+
 find /usr/lib/ -name '*.pyc' -delete
 find /usr/lib/ -name '*.pyo' -delete
 mkdir -p /var/log/audit
 mkdir -p /var/log/archive
 chmod 700 /var/log/audit
 chmod 700 /var/log/archive
-#cat /tmp/.reldate > /etc/BUILDDATE
-uname -a > /etc/BUILDHOST
 rm -rf /etc/portage/rootfs
-rm -rf "/usr/share/applications/links_-g_%u-links-2.desktop"
 rm -rf /etc/portage/patches
+rm -f "/usr/share/applications/links_-g_%u-links-2.desktop"
+rm -f /usr/share/applications/org.octave.Octave.desktop
 # left-over from ipsvd
 rm -rf /html
 
+echo /usr/lib64 >> /etc/ld.so.conf
 echo /usr/lib/samba >> /etc/ld.so.conf
 echo /usr/lib/libreoffice/program >> /etc/ld.so.conf
 /sbin/ldconfig
