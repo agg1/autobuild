4d5a164ebfdb81c7825c3a45b721f482e736c66b
autobuild <autobuild@padwalker.com>
2019-11-29 13:34:02 +0000

    unmask script interpreter packages on finalized release

--- etc/portage/finalize.sh
+++ etc/portage/finalize.sh
@@ -15,12 +15,9 @@ chmod 640 /etc/doas.conf
 rm -f /etc/portage/finalize.sh
 rm -rf /etc/portage/cscriptoverlay
 rm -rf /etc/portage/cdtar
-#rm -rf /etc/portage/env
 rm -rf /etc/portage/initramfs
 rm -rf /etc/portage/rootfs
-#rm -rf /etc/portage/package*
 rm -rf /etc/portage/patches
-#rm -rf /etc/portage/profile
 rm -f /etc/portage/genkernel.conf
 rm -f /etc/portage/catalyst*
 rm -f /etc/portage/kconfig*
@@ -30,8 +27,6 @@ rm -f /usr/share/applications/org.octave.Octave.desktop
 rm -f /usr/share/applications/spyder-spyder.desktop
 rm -f /usr/share/applications/spyder.desktop
 rm -rf /html
-#rm -f /var/db/pkg/dev-libs/lzx-*/environment.bz2
-#rm -f /var/db/pkg/dev-libs/lzxp-*/environment.bz2
 rm -f /var/db/pkg/app-arch/lzx-*/environment.bz2
 
 chgrp input /usr/bin/Xorg 2>/dev/null
@@ -42,5 +37,21 @@ chmod g+s /usr/bin/Xorg 2>/dev/null
 rm -f /etc/init.d/agetty.tty*
 
 rm -f /.catalyst_lock
+
+echo 'dev-lang/lua'			>> /etc/portage/package.unmask/override
+echo 'dev-lang/luajit'      >> /etc/portage/package.unmask/override
+echo 'dev-lang/ruby'		>> /etc/portage/package.unmask/override
+echo 'dev-lang/rust'		>> /etc/portage/package.unmask/override
+echo 'dev-lang/php'			>> /etc/portage/package.unmask/override
+echo 'dev-lang/go'			>> /etc/portage/package.unmask/override
+echo 'virtual/jdk'			>> /etc/portage/package.unmask/override
+echo 'virtual/jre'			>> /etc/portage/package.unmask/override
+echo 'dev-java/icedtea-bin'	>> /etc/portage/package.unmask/override
+echo 'dev-lang/mono'		>> /etc/portage/package.unmask/override
+echo 'dev-lang/ocaml'		>> /etc/portage/package.unmask/override
+echo 'dev-lang/vala'		>> /etc/portage/package.unmask/override
+echo 'dev-lang/tcl'			>> /etc/portage/package.unmask/override
+echo 'dev-lang/tk'			>> /etc/portage/package.unmask/override
+echo 'dev-lang/orc'			>> /etc/portage/package.unmask/override
 
 true
